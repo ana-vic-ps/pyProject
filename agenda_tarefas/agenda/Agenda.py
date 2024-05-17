fe19915e6140a4b1079dd4f5aef0657cc563e28e
Ana Victoria Pereira de Souza <ana.victoria@a.ucb.br>
2024-05-16 22:41:21 -0300

    adicionando comentarios

--- agenda_tarefas/agenda/Agenda.py
+++ agenda_tarefas/agenda/Agenda.py
@@ -1,36 +1,38 @@
-from agenda.Tarefa import Tarefa
 import os
+from agenda.Tarefa import Tarefa
 
 
 class Agenda:
     def __init__(self):
-        self.tarefas = []
+        self.tarefas = [] # Inicializa a lista de tarefas
 
-    def adicionar_tarefa(self, nova_tarefa):
-        self.tarefas.append(nova_tarefa)
-        self.salvar_agenda("agenda_tarefas.txt")
-        print("Tarefa adicionada com sucesso!")
+    def adicionar_tarefa(self, nova_tarefa, show_message=True):
+        self.tarefas.append(nova_tarefa) # Adiciona a nova tarefa à lista
+        self.salvar_agenda("agenda_tarefas.txt") # Salva a agenda no arquivo
+        if show_message:
+            print("Tarefa adicionada com sucesso!")
 
     def remover_tarefa(self, tarefa):
         if tarefa in self.tarefas:
-            self.tarefas.remove(tarefa)
-            self.salvar_agenda("agenda_tarefas.txt")  
-            print("Tarefa removida com sucesso!")
+            self.tarefas.remove(tarefa) # Remove a tarefa da lista
+            self.salvar_agenda("agenda_tarefas.txt") # Salva a agenda atualizada no arquivo
+            print("\nTarefa removida com sucesso!")
         else:
             print("Tarefa não encontrada.")
 
-
     def exibir_tarefas(self):
         if not self.tarefas:
-            print("Nenhuma tarefa na agenda.")
+            print("\nNenhuma tarefa na agenda.")
         else:
             print("Tarefas na agenda:\n")
             for tarefa in self.tarefas:
-                print(f'Nome: {tarefa.nome}\n Início: {tarefa.data_inicio}\n Término: {tarefa.data_termino}\n Prioridade: {tarefa.prioridade}\n Estado: {tarefa.estado_tarefa}\n')
+                print(f'Nome: {tarefa.nome}\nInício: {tarefa.data_inicio}\nTérmino: {tarefa.data_termino}\nPrioridade: {tarefa.prioridade}\nEstado: {tarefa.estado_tarefa}\n')
 
     def salvar_agenda(self, nome_arquivo):
         try:
+             # Escreve tarefa por tarefa no arquivo
             with open(nome_arquivo, 'w') as file:
+                file.write("[Nome da tarefa, Data de Inicio da tarefa, Data de Termino da tarefa, Prioridade, Estado]\n\n")
                 for tarefa in self.tarefas:
                     file.write(f"{tarefa.nome}, {tarefa.data_inicio}, {tarefa.data_termino}, {tarefa.prioridade}, {tarefa.estado_tarefa}\n")
         except IOError:
@@ -38,16 +40,19 @@ class Agenda:
 
     def carregar_agenda(self, nome_arquivo):
         try:
-            # Verifica se o arquivo existe, se não existir, cria um novo
+             # Cria o arquivo se ele não existir
             if not os.path.exists(nome_arquivo):
                 with open(nome_arquivo, 'w'):
-                    pass  # Cria o arquivo vazio
-                    print(f"Arquivo {nome_arquivo} criado.")
+                    pass
+                print(f"Arquivo {nome_arquivo} criado.")
             with open(nome_arquivo, 'r') as file:
+                next(file)
+                next(file)
                 for line in file:
-                    dados_tarefa = line.strip().split(',')
+                    dados_tarefa = line.strip().split(', ')
                     nome, data_inicio, data_termino, prioridade, estado_tarefa = dados_tarefa
+                    # Cria uma nova tarefa a partir dos dados do arquivo
                     nova_tarefa = Tarefa(nome, data_inicio, data_termino, prioridade, estado_tarefa)
-                    self.adicionar_tarefa(nova_tarefa)
+                    self.adicionar_tarefa(nova_tarefa, show_message=False) # Adiciona a nova tarefa à lista de tarefas
         except Exception as e:
             print("Erro ao carregar a agenda:", e)
--- agenda_tarefas/main.py
+++ agenda_tarefas/main.py
@@ -22,13 +22,14 @@ def definindo_prioridade_tarefa():
         else:
             print("Opção inválida. Tente novamente.")
 
+
 def definindo_estado_tarefa():
-    estado_tarefa = ['NÃO INICIADA', 'EM PROGESSO', 'EM PAUSA', 'CONCLUÍDA',]
-    print("\nSelecione o estada da tarefa: ")
+    estado_tarefa = ['NÃO INICIADA', 'EM PROGRESSO', 'EM PAUSA', 'CONCLUÍDA']
+    print("\nSelecione o estado da tarefa: ")
     for index, estado in enumerate(estado_tarefa, 1):
         print(f'{index}. {estado}')
-    while True: 
-        opcao = input ('Escolha uma opção (1-4): ')
+    while True:
+        opcao = input('Escolha uma opção (1-4): ')
         if opcao in ['1', '2', '3', '4']:
             return estado_tarefa[int(opcao) - 1]
         else:
@@ -36,47 +37,44 @@ def definindo_estado_tarefa():
 
 
 def main():
-
     agenda = Agenda()
-    agenda.carregar_agenda("agenda_tarefas.txt")
-    
+    agenda.carregar_agenda("agenda_tarefas.txt") # Carrega as tarefas do arquivo
+
     while True:
-        
-        menu()
+        menu() # Exibe o menu
         opcao = input('O que deseja realizar? ')
 
         if opcao == '1':
-            
             nome = input('Digite o nome da tarefa: ')
-            data_inicio = input('Digite a data de inicio da tarefa: ')
-            data_termino = input('Digite a data de termino da tarefa: ')
+            data_inicio = input('Digite a data de início da tarefa: ')
+            data_termino = input('Digite a data de término da tarefa: ')
             prioridade = definindo_prioridade_tarefa()
             estado_tarefa = definindo_estado_tarefa()
             nova_tarefa = Tarefa(nome, data_inicio, data_termino, prioridade, estado_tarefa)
-            agenda.adicionar_tarefa(nova_tarefa)
-            
-            
+            agenda.adicionar_tarefa(nova_tarefa) # Adiciona a nova tarefa à agenda
+
         elif opcao == '2':
             nome = input('Digite o nome da tarefa a ser removida: ')
             tarefa_a_remover = None
+             # Procura pela tarefa na lista de tarefas
             for tarefa in agenda.tarefas:
                 if tarefa.nome == nome:
                     tarefa_a_remover = tarefa
                     break
             if tarefa_a_remover:
-                agenda.remover_tarefa(tarefa_a_remover)
+                agenda.remover_tarefa(tarefa_a_remover) # Remove a tarefa da agenda
             else:
-                print("Tarefa não encontrada.")
-                
+                print("\nTarefa não encontrada.")
+
         elif opcao == '3':
-            agenda.exibir_tarefas()
-            
+            agenda.exibir_tarefas() # Exibe todas as tarefas
+
         elif opcao == '4':
             break
-        
+
         else:
-            print("Opção inválida. Tente novamente.")
-                
+            print("\nOpção inválida. Tente novamente.")
+
 
 if __name__ == "__main__":
     main()
